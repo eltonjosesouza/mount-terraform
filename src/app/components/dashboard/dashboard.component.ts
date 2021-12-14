1dbe4a46044466f0a68d6dc8c57c483e4f767f17
Elton José <elton.jose.souza@gmail.com>
2021-12-13 22:12:02 -0300

    Ajustes de layout

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -10,9 +10,21 @@ import { ResourceStorageService } from 'src/app/services/resource-storage.servic
 export class DashboardComponent implements OnInit {
   /** Based on the screen size, switch from standard to one column per row */
 
+
+  exampleResource: string = "";
+  editorMode = "editor";
+  editorOptions = {
+    showPreviewPanel: false,   // Show preview panel, Default is true
+    showBorder: true,         // Show editor component's border. Default is true
+    hideIcons: ['Bold', 'Italic', 'Heading', 'Reference', 'Link', 'Image', 'Ul', 'Ol', 'Code', 'TogglePreview', 'FullScreen'],
+    scrollPastEnd: 0,      // The option for ace editor. Default is 0
+    resizable: true,           // Allow resize the editor
+  }
+
   constructor(private serviceResource: ResourceStorageService, private router: Router,) { }
 
   ngOnInit(): void {
+    this.getOnlyContentOnResourceList();
   }
 
   clearResourceList() {
@@ -22,10 +34,37 @@ export class DashboardComponent implements OnInit {
     });
   }
 
-
-
   exportTerraformFiles() {
+    this.writeContents(this.exampleResource, 'main' + '.tf', 'text/plain');
+  }
 
+  getOnlyContentOnResourceList() {
+    this.exampleResource = this.escapeAllEspecialCharacters(
+      this.transformMapInString(
+        this.serviceResource.getResourceList().map(x => x.content)));
+    return this.exampleResource;
+  }
+
+  transformMapInString(list: any[]): string {
+    let string = "";
+    list.forEach(element => {
+      string += element + "\n";
+    });
+    return string;
+  }
+
+  escapeAllEspecialCharacters(string: string): string {
+    return string;//.replace(/\n\r?/g, '').replace(/\"/g,"\'");
+  }
+
+
+  writeContents(content: BlobPart, fileName: string, contentType: string) {
+    console.log(content);
+    var a = document.createElement('a');
+    var file = new Blob([content], { type: contentType });
+    a.href = URL.createObjectURL(file);
+    a.download = fileName;
+    a.click();
   }
 
 }
